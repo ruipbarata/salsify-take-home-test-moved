149a6b69c1cd3a1840996c405e2e89f631259e64
Rui Barata <ruipbarata@gmail.com>
2025-02-08 23:10:39 +0000

    wip - locks

--- app/services/file_reader_chunks_service.rb
+++ app/services/file_reader_chunks_service.rb
@@ -20,6 +20,9 @@ class FileReaderChunksService
 
     # Try retrieving the line from cache; otherwise, read from the file
     Rails.cache.fetch("#{LINE_CACHE_KEY_PREFIX}#{index}") do
+      offset = fetch_offset(index)
+      return unless offset
+
       read_line(index)
     end
   end
@@ -27,12 +30,9 @@ class FileReaderChunksService
   private
 
   # Reads a specific line from the file
-  # @param index [Integer] The index of the line to read
-  # @return [String, nil] The requested line or nil if it does not exist
-  def read_line(index)
-    offset = fetch_offset(index)
-    return unless offset
-
+  # @param offset [Integer] The start position of the line to read
+  # @return [String, nil] The requested line or nil if it is out of bounds
+  def read_line(offset)
     File.open(@file_path, "r") do |file|
       file.seek(offset)
       file.eof? ? nil : file.readline.chomp
@@ -45,10 +45,16 @@ class FileReaderChunksService
   def fetch_offset(index)
     chunk_index = index / CHUNK_SIZE
 
+    debugger if index == 10000000
+
     # Retrieve the offsets for the corresponding chunk from cache, generating it if necessary
-    offsets = Rails.cache.fetch("#{LINE_OFFSETS_CACHE_PREFIX}#{chunk_index}") do
-      load_and_cache_blocks(chunk_index)
+    offsets = Rails.cache.fetch("#{LINE_OFFSETS_CACHE_PREFIX}#{chunk_index}")
+
+    if offsets.nil?
+      offsets = load_and_cache_blocks(index, chunk_index)
     end
+
+    debugger if index == 10000000
 
     offsets[index % CHUNK_SIZE]
   end
@@ -56,40 +62,53 @@ class FileReaderChunksService
   # Processes and stores offsets for chunks of the file
   # @param target_block_index [Integer] The index of the chunk being processed
   # @return [Array<Integer>] The list of offsets for the target chunk
-  def load_and_cache_blocks(target_chunk_index)
-    # Retrieve the next block index to determine the starting position
-    next_chunk_index_to_process = fetch_next_chunk_index_to_process
-    last_chunk_index_to_process = next_chunk_index_to_process == 0 ? 0 : next_chunk_index_to_process - 1
-    start_offset = next_chunk_index_to_process == 0 ? 0 : fetch_last_block_offset(last_chunk_index_to_process)
+  def load_and_cache_blocks(index, target_chunk_index)
+    lock_value = acquire_offset_lock(target_chunk_index)
 
-    target_block_offsets = []
-
-    File.open(@file_path, "r") do |file|
-      file.seek(start_offset)
-
-      (next_chunk_index_to_process..target_chunk_index).each do |curr_chunk_index|
-        block_offsets = []
-
-        while block_offsets.size < CHUNK_SIZE
-          break if file.eof?
-
-          # Ensure the first line is correctly handled
-          file.readline unless start_offset == 0 && block_offsets.empty? && target_block_offsets.empty?
-
-          block_offsets << file.pos
-        end
-
-        Rails.cache.write("#{LINE_OFFSETS_CACHE_PREFIX}#{curr_chunk_index}", block_offsets)
-
-        target_block_offsets = block_offsets
-
-        break if file.eof?
-      end
+    unless lock_value
+      debugger if index == 10000000
+      # Aqui podemos colocar um mecanismo de retry ou sleep para esperar o lock ser liberado
+      sleep(1)
+      return fetch_offset(index)
     end
 
-    Rails.cache.write("#{LINE_OFFSETS_CACHE_PREFIX}next_block", target_chunk_index + 1)
+    begin
+      # Retrieve the next block index to determine the starting position
+      next_chunk_index_to_process = fetch_next_chunk_index_to_process
+      last_chunk_index_to_process = next_chunk_index_to_process == 0 ? 0 : next_chunk_index_to_process - 1
+      start_offset = next_chunk_index_to_process == 0 ? 0 : fetch_last_block_offset(last_chunk_index_to_process)
 
-    target_block_offsets
+      target_block_offsets = []
+
+      File.open(@file_path, "r") do |file|
+        file.seek(start_offset)
+
+        (next_chunk_index_to_process..target_chunk_index).each do |curr_chunk_index|
+          block_offsets = []
+
+          while block_offsets.size < CHUNK_SIZE
+            break if file.eof?
+
+            # Ensure the first line is correctly handled
+            file.readline unless start_offset == 0 && block_offsets.empty? && target_block_offsets.empty?
+
+            block_offsets << file.pos
+          end
+
+          Rails.cache.write("#{LINE_OFFSETS_CACHE_PREFIX}#{curr_chunk_index}", block_offsets)
+
+          target_block_offsets = block_offsets
+
+          break if file.eof?
+        end
+      end
+
+      Rails.cache.write("#{LINE_OFFSETS_CACHE_PREFIX}next_block", target_chunk_index + 1)
+
+      target_block_offsets
+    ensure
+      release_offset_lock
+    end
   end
 
   # Retrieves the index of the next block to be processed
@@ -103,5 +122,23 @@ class FileReaderChunksService
   # @return [Integer] The last offset of the specified block, or 0 if none exists
   def fetch_last_block_offset(chunk_index)
     Rails.cache.read("#{LINE_OFFSETS_CACHE_PREFIX}#{chunk_index}")&.last || 0
+  end
+
+  def acquire_offset_lock(chunk_index)
+    lock_key = "chunk_lock"
+    lock_value = chunk_index
+
+    # Tenta adquirir o lock, se não existir, irá criar um com um TTL (tempo de expiração)
+    locked = Rails.cache.write(lock_key, lock_value, unless_exist: true, expires_in: 10.minutes)
+
+    locked
+  end
+
+  # Método para liberar o lock do bloco
+  def release_offset_lock
+    debugger
+    lock_key = "chunk_lock"
+
+    Rails.cache.delete(lock_key)
   end
 end
